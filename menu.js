d936ac09ec16981b882b2ad9e69725f45745992d
Endermanboy <160394416+Endermanboy@users.noreply.github.com>
2026-04-20 11:48:34 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded',()=>{
      <a href="https://endermanboy.github.io/noppa-ai/Kuva%20korruptori/">
   <button>Kuva korruptori</button>
 </a>
+<a href="https://endermanboy.github.io/noppa-ai/Bonzi%20Buddy%20TTS/">
+  <button>Bonzi Buddy TTS</button>
+</a>
     </div>`;
   document.body.appendChild(menu);
   const btn=document.getElementById('openMenu');
