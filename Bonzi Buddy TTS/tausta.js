3537f2db33473be5cc7d2fc527202004abfeb18c
Endermanboy <160394416+Endermanboy@users.noreply.github.com>
2026-04-24 15:58:42 +0300

    Add background image to tausta.js on load

---
+++ Bonzi Buddy TTS/tausta.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.backgroundImage = "url('images/tausta.png')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+});
