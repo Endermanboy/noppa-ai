0d8445bd1ec704188799aad8b84636f9bfca1123
Endermanboy <160394416+Endermanboy@users.noreply.github.com>
2026-08-10 21:39:18 +0300

    Add files via upload

---
+++ slot machine/menu.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  const menu=document.createElement('div');
+  menu.innerHTML=`
+    <button id="openMenu" style="position:fixed;left:10px;top:10px;z-index:1000;padding:10px;border:none;border-radius:10px;cursor:pointer;">☰</button>
+    <div id="sideMenu" style="position:fixed;left:-220px;top:0;width:220px;height:100%;background:#1a1a1a;color:white;padding:20px;transition:0.3s;z-index:999;">
+      <h2>Valikko</h2>
+     <a href="https://endermanboy.github.io/noppa-ai/Kuva%20korruptori/">
+  <button style="margin-bottom:10px;">Kuva korruptori</button>
+</a>
+
+<a href="https://endermanboy.github.io/noppa-ai/Bonzi%20Buddy%20TTS/">
+  <button style="margin-bottom:10px;">Bonzi Buddy TTS</button>
+</a>
+
+<a href="https://endermanboy.github.io/noppa-ai/Desibelimittari/">
+  <button style="margin-bottom:10px;">Desibelimittari</button>
+</a>
+
+<a href="https://endermanboy.github.io/noppa-ai/slot%20machine/">
+<button style="margin-bottom:10px;">Lets go gambling</button>
+</a>
+
+    </div>`;
+  document.body.appendChild(menu);
+  const btn=document.getElementById('openMenu');
+  const side=document.getElementById('sideMenu');
+  let open=false;
+  btn.onclick=()=>{open=!open;side.style.left=open?'0':'-220px';};
+});
